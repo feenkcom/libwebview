2714b7334298d92b445d48c92972e7098dd8cb4b
Aliaksei Syrel <alex.syrel@gmail.com>
2024-06-24 22:24:32 +0200

    fix the builder main

--- libwebview-builder/src/main.rs
+++ libwebview-builder/src/main.rs
@@ -1,8 +1,8 @@
 use std::error::Error;
 
-use libwebview_builder::latest_libwinit;
+use libwebview_builder::latest_libwebview;
 use shared_library_builder::build_standalone;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    build_standalone(|_| Ok(Box::new(latest_libwinit())))
+    build_standalone(|_| Ok(Box::new(latest_libwebview())))
 }
