daeebca47a8addae3d3b44e5277fd15afb839201
Aliaksei Syrel <alex.syrel@gmail.com>
2024-07-02 14:09:54 +0200

    add webview_init_gtk

--- libwebview/src/lib.rs
+++ libwebview/src/lib.rs
@@ -12,3 +12,19 @@ pub extern "C" fn webview_test() -> bool {
 pub extern "C" fn webview_init_logger() {
     env_logger::init();
 }
+
+#[no_mangle]
+pub extern "C" fn webview_init_gtk() {
+    #[cfg(not(any(
+        target_os = "windows",
+        target_os = "macos",
+        target_os = "ios",
+        target_os = "android"
+    )))]
+    {
+        use value_box::ReturnBoxerResult;
+        gtk::init()
+            .map_err(|error| anyhow::anyhow!(error).into())
+            .log();
+    }
+}
